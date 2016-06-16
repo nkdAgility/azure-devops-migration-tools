96d09da9736cdc96188cd2942b22ab907627d0c4
Martin Hinshelwood (nkdAgility) <martin@nkdagility.com>
2016-06-16 13:36:56 +0200

    Updated Program for customer

--- TfsWitMigrator.Console/Program.cs
+++ TfsWitMigrator.Console/Program.cs
@@ -35,17 +35,58 @@ namespace ConsoleApplication1
             MigrationEngine me = new MigrationEngine();
 
 
-            me.SetSource(new TeamProjectContext(new Uri("https://sdd2016.visualstudio.com/"), "DemoProj1"));
+            //me.SetSource(new TeamProjectContext(new Uri("https://sdd2016.visualstudio.com/"), "DemoProj1"));
             me.SetTarget(new TeamProjectContext(new Uri("https://sdd2016.visualstudio.com/"), "test1"));
             me.SetReflectedWorkItemIdFieldName("ReflectedWorkItemId");
-            me.AddWorkItemTypeDefinition("User Story", new DescreteWitdMapper("User Story"));
-            me.AddWorkItemTypeDefinition("Product Backlog Item", new DescreteWitdMapper("Product Backlog Itemy"));
-            me.AddWorkItemTypeDefinition("Task", new DescreteWitdMapper("Task"));
-            me.AddWorkItemTypeDefinition("Bug", new DescreteWitdMapper("Bug"));
 
-            me.AddProcessor<NodeStructuresMigrationContext>();
-            me.AddProcessor<WorkItemMigrationContext>();
-           // me.AddProcessor<LinkMigrationContext>();
+            Dictionary<string, string> stateMapping = new Dictionary<string, string>();
+            stateMapping.Add("New", "New");
+            stateMapping.Add("Approved", "New");
+            stateMapping.Add("Committed", "Active");
+            stateMapping.Add("In Progress", "Active");
+            stateMapping.Add("To Do", "New");
+            stateMapping.Add("Done", "Closed");
+            me.AddFieldMap("*", new FieldValueMap("System.State", "System.State", stateMapping));
+
+            me.AddFieldMap("*", new FieldToTagFieldMap("System.State", "ScrumState:{0}"));
+            me.AddFieldMap("*", new FieldToFieldMap("Microsoft.VSTS.Common.BacklogPriority", "Microsoft.VSTS.Common.StackRank"));
+            me.AddFieldMap("*", new FieldToFieldMap("Microsoft.VSTS.Scheduling.Effort", "Microsoft.VSTS.Scheduling.StoryPoints"));
+            me.AddFieldMap("*", new FieldMergeMap("System.Description", "Microsoft.VSTS.Common.AcceptanceCriteria", "System.Description", @"{0} <br/><br/><h3>Acceptance Criteria</h3>{1}"));
+            me.AddFieldMap("*", new FieldToFieldMap("Microsoft.VSTS.CMMI.AcceptanceCriteria", "COMPANY.DEVISION.Analysis"));
+
+
+            //me.AddFieldMap("Requirement", new FieldToTagFieldMap("COMPANY.PRODUCT.ReqType", "ReqType:{0}"));
+            //me.AddFieldMap("Requirement", new FieldToTagFieldMap("COMPANY.PRODUCT.Theme", "Theme:{0}"));
+            //me.AddFieldMap("Requirement", new FieldToTagFieldMap("COMPANY.PRODUCT.FeatureDocumented", "Documented:{0}"));
+            //me.AddFieldMap("Requirement", new FieldToFieldMap("Microsoft.VSTS.CMMI.ImpactAssessmentHtml", "COMPANY.DEVISION.Analysis"));
+            // me.AddFieldMap("Requirement", new RegexFieldMap("COMPANY.PRODUCT.Release", "COMPANY.DEVISION.Release", @"PRODUCT (\d{4}).\d{1}"));
+            //me.AddFieldMap("Requirement", new RegexFieldMap("COMPANY.PRODUCT.Release", "COMPANY.DEVISION.MinorReleaseVersion", @"PRODUCT \d{4}.(\d{1})"));
+            //me.AddFieldMap("Requirement", new FieldToTagFieldMap("COMPANY.PRODUCT.ReqType", "ReqType:{0}"));
+            //me.AddFieldMap("Requirement", new FieldToTagFieldMap("COMPANY.PRODUCT.ReqType", "ReqType:{0}"));
+
+            //me.AddFieldMap("User Story", new FieldToTagFieldMap("COMPANY.PRODUCT.ReqType", "ReqType:{0}"));
+            //me.AddFieldMap("User Story", new FieldToTagFieldMap("COMPANY.PRODUCT.PortfolioAcceptance", "Portfolio:{0}"));
+            //me.AddFieldMap("User Story", new FieldToFieldMap("Microsoft.VSTS.CMMI.ReqType", "COMPANY.DEVISION.UserStoryType"));
+            //me.AddFieldMap("User Story", new FieldToFieldMap("Microsoft.VSTS.CMMI.Stakeholder", "COMPANY.DEVISION.Stakeholder"));
+            //
+            //me.AddFieldMap("User Story", new RegexFieldMap("COMPANY.PRODUCT.Snapshot", "COMPANY.DEVISION.PreCommercialVersion", @"Snapshot ([0-9])", "Alpha 0$1"));
+            //me.AddFieldMap("User Story", new RegexFieldMap("COMPANY.PRODUCT.Snapshot", "COMPANY.DEVISION.PreCommercialVersion", @"Snapshot 10", "Alpha 10"));
+
+
+            me.AddProcessor(new WorkItemUpdate(me, @" "));
+
+
+
+
+
+            //Me.AddWorkItemTypeDefinition("User Story", new DescreteWitdMapper("User Story"));
+            // me.AddWorkItemTypeDefinition("Product Backlog Item", new DescreteWitdMapper("Product Backlog Itemy"));
+            // me.AddWorkItemTypeDefinition("Task", new DescreteWitdMapper("Task"));
+            // me.AddWorkItemTypeDefinition("Bug", new DescreteWitdMapper("Bug"));
+
+            // me.AddProcessor<NodeStructuresMigrationContext>();
+            // me.AddProcessor<WorkItemMigrationContext>();
+            // me.AddProcessor<LinkMigrationContext>();
             //me.AddProcessor<AttachementExportMigrationContext>();
             // me.AddProcessor<AttachementImportMigrationContext>();
 
