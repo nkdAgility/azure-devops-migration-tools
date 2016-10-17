115c2855ecf8086113f662f5b87acfb2d450e73c
Martin Hinshelwood (nkdAgility) <martin@nkdagility.com>
2016-10-17 15:20:00 -0500

    Updated messages to make a valid package -semver:minor

--- TfsWitMigrator.Console/Chocolatey/chocolateyInstall.ps1
+++ TfsWitMigrator.Console/Chocolatey/chocolateyInstall.ps1
@@ -9,10 +9,7 @@ try {
   }
   
   Install-ChocolateyZipPackage 'vstssyncmigration' 'https://github.com/nkdAgility/vsts-sync-migration/releases/download/#{GITVERSION.FULLSEMVER}#/vstsbulkeditor-#{GITVERSION.FULLSEMVER}#.zip' $vstssyncmigrationpath
-
   write-host 'VSTS Sync Migration has been installed. Call `vstssyncmigration` from the command line to see options. You may need to close and reopen the command shell.'
-  Write-ChocolateySuccess 'vstssyncmigration'
 } catch {
-  Write-ChocolateyFailure 'vstssyncmigration' $($_.Exception.Message)
   throw 
 }
