a9eb62cb25b0f091e7a5895e76b44971353fc307
Martin Hinshelwood (nkdAgility) <martin@nkdagility.com>
2016-10-21 12:37:49 -0500

    Updated chocoaltey install to use Cehcksum +semver:minor

--- TfsWitMigrator.Console/Chocolatey/chocolateyInstall.ps1
+++ TfsWitMigrator.Console/Chocolatey/chocolateyInstall.ps1
@@ -6,5 +6,5 @@ if(test-path $vstssyncmigrationpath) {
   Remove-Item "$($vstssyncmigrationpath)\*" -recurse -force
 }
 
-Install-ChocolateyZipPackage 'vsts-sm' 'https://github.com/nkdAgility/vsts-sync-migration/releases/download/#{GITVERSION.FULLSEMVER}#/vstsbulkeditor-#{GITVERSION.FULLSEMVER}#.zip' $vstssyncmigrationpath
+Install-ChocolateyZipPackage 'vsts-sm' 'https://github.com/nkdAgility/vsts-sync-migration/releases/download/#{GITVERSION.FULLSEMVER}#/vstsbulkeditor-#{GITVERSION.FULLSEMVER}#.zip' $vstssyncmigrationpath -Checksum #{Chocolatey.FileHash}# -ChecksumType SHA256
 write-host 'VSTS Sync Migration has been installed. Call `vsts-sm` from the command line to see options. You may need to close and reopen the command shell.'
