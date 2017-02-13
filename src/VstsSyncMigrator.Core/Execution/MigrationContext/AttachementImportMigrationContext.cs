f8da661ca05dc2e8856cc6ec9880747afd25b302
Richard Fennell <richard@blackmarble.com>
2017-02-13 13:19:27 +0000

    Merged PR 92: Added a check for failed imports
    Added a check for failed imports
    This addresses Github issues #9
    
    Related work items: #5004

--- src/VstsSyncMigrator.Core/Execution/MigrationContext/AttachementImportMigrationContext.cs
+++ src/VstsSyncMigrator.Core/Execution/MigrationContext/AttachementImportMigrationContext.cs
@@ -36,28 +36,38 @@ namespace VstsSyncMigrator.Engine
             List<string> files = System.IO.Directory.EnumerateFiles(exportPath).ToList<string>();
             WorkItem targetWI = null;
             int current = files.Count;
+            int failures = 0;
             foreach (string file in files)
             {
                 string fileName = System.IO.Path.GetFileName(file);
-                string reflectedID =fileName.Split('#')[0].Replace('+', ':').Replace("--","/");
-                 targetWI = targetStore.FindReflectedWorkItemByReflectedWorkItemId(reflectedID, me.ReflectedWorkItemIdFieldName);
-                if (targetWI != null)
+                try
                 {
-                    Trace.WriteLine(string.Format("{0} of {1} - Import {2} to {3}", current, files.Count, fileName, targetWI.Id));
-                    Attachment a = new Attachment(file);
-                    targetWI.Attachments.Add(a);
-                    
-                    targetWI.Save();
-                    System.IO.File.Delete(file);
-                } else
+                    string reflectedID = fileName.Split('#')[0].Replace('+', ':').Replace("--", "/");
+                    targetWI = targetStore.FindReflectedWorkItemByReflectedWorkItemId(reflectedID, me.ReflectedWorkItemIdFieldName);
+                    if (targetWI != null)
+                    {
+                        Trace.WriteLine(string.Format("{0} of {1} - Import {2} to {3}", current, files.Count, fileName, targetWI.Id));
+                        Attachment a = new Attachment(file);
+                        targetWI.Attachments.Add(a);
+
+                        targetWI.Save();
+                        System.IO.File.Delete(file);
+                    }
+                    else
+                    {
+                        Trace.WriteLine(string.Format("{0} of {1} - Skipping {2} to {3}", current, files.Count, fileName, 0));
+                    }
+                } catch (FileAttachmentException ex)
                 {
-                    Trace.WriteLine(string.Format("{0} of {1} - Skipping {2} to {3}", current, files.Count, fileName, 0));
+                    // Probably due to attachment being over size limit
+                    Trace.WriteLine(ex.Message) ;
+                    failures++;
                 }
                 current--;
             }
             //////////////////////////////////////////////////
             stopwatch.Stop();
-            Console.WriteLine(@"EXPORT DONE in {0:%h} hours {0:%m} minutes {0:s\:fff} seconds", stopwatch.Elapsed);
+            Console.WriteLine(@"IMPORT DONE in {0:%h} hours {0:%m} minutes {0:s\:fff} seconds - {1} Files imported, {2} Failures", stopwatch.Elapsed, (files.Count- failures), failures);
         }
 
     }
