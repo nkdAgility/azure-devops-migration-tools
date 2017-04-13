90df48e5c24709914042ff1543b4147f0ef199da
Harald Köstinger <harald.koestinger@innoforce.com>
2017-04-13 07:52:40 +0200

    Added the possibility to specify a repository name on the target project, as the source repository name may not be present (or suitable) on the target project. Updated documentation for this purpose.

--- src/VstsSyncMigrator.Core/Configuration/Processing/FixGitCommitLinksConfig.cs
+++ src/VstsSyncMigrator.Core/Configuration/Processing/FixGitCommitLinksConfig.cs
@@ -8,6 +8,8 @@ namespace VstsSyncMigrator.Engine.Configuration.Processing
         public string TargetRepository { get; set; }
         public bool Enabled { get; set; }
 
+        public string TargetRepository { get; set; }
+
         public Type Processor
         {
             get { return typeof(FixGitCommitLinks); }
