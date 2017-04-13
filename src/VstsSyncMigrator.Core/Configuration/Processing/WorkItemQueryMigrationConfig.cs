7433a3252979fab4c42239b880d2bd45bb62f0f4
Harald Köstinger <harald.koestinger@innoforce.com>
2017-04-13 08:35:20 +0200

    Fixing documentation for work item revisions replay context. Including safety check to not allow running WorkItemMigrationContext and WorkItemRevisionReplayMigrationContext at the same time, as they are not "compatible" with each other.

--- src/VstsSyncMigrator.Core/Configuration/Processing/WorkItemQueryMigrationConfig.cs
+++ src/VstsSyncMigrator.Core/Configuration/Processing/WorkItemQueryMigrationConfig.cs
@@ -1,51 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VstsSyncMigrator.Engine.Configuration.Processing
 {
     public class WorkItemQueryMigrationConfig : ITfsProcessingConfig
     {
         /// <summary>
-        /// Is this processor enabled
-        /// </summary>
-        public bool Enabled { get; set; }
-
-        /// <summary>
-        /// Do we add the source project name into the folder path
-        /// </summary>
-        public bool PrefixProjectToNodes { get; set; }
-
-        /// <summary>
-        ///  The name of the shared folder, setting the default name
+        ///     The name of the shared folder, setting the default name
         /// </summary>
         private string sharedFolderName = "Shared Queries";
 
         /// <summary>
-        ///  The name of the shared folder, made a parameter incase it every needs to be edited
+        ///     Do we add the source project name into the folder path
+        /// </summary>
+        public bool PrefixProjectToNodes { get; set; }
+
+        /// <summary>
+        ///     The name of the shared folder, made a parameter incase it every needs to be edited
         /// </summary>
         public string SharedFolderName
         {
-            get
-            {
-                return this.sharedFolderName;
-            }
-            set
-            {
-                this.sharedFolderName = value;
-            }
+            get { return sharedFolderName; }
+            set { sharedFolderName = value; }
         }
+
+        /// <summary>
+        ///     Is this processor enabled
+        /// </summary>
+        public bool Enabled { get; set; }
 
         public Type Processor
         {
-            get
-            {
-                return typeof(WorkItemQueryMigrationContext);
-            }
+            get { return typeof(WorkItemQueryMigrationContext); }
         }
 
+        /// <inheritdoc />
+        public bool IsProcessorCompatible(IReadOnlyList<ITfsProcessingConfig> otherProcessors)
+        {
+            return true;
+        }
     }
 }
-
