26d54d18c546614f44b7f788ce846ebb6f35a2d0
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2019-09-23 19:50:01 +0200

    Updated powershell from fullsemver to SemVer

--- src/VstsSyncMigrator.Chocolatey/chocolateyUninstall.ps1
+++ src/VstsSyncMigrator.Chocolatey/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $toolsLoc = Get-ToolsLocation
 $vstssyncmigrationpath =Join-Path -Path $toolsLoc -ChildPath "\VSTSSyncMigration"
 
-Uninstall-ChocolateyZipPackage 'vstssyncmigrator' 'vstssyncmigrator-#{GITVERSION.FULLSEMVER}#.zip'
+Uninstall-ChocolateyZipPackage 'vstssyncmigrator' 'vstssyncmigrator-#{GITVERSION.SEMVER}#.zip'
 
 write-host 'VSTS Sync Migration has been uninstalled.'
