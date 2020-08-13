af436127442124777f991958ed9183822d800342
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-08-13 13:10:20 +0100

    Updated to change how detects doneness (#529)
    * Updated to change how detects doneness
    
    * Updated to reflect exeption process
    
    * Update vss-extension.json

--- src/VstsSyncMigrator.Core/Execution/FieldMaps/FieldMergeMap.cs
+++ src/VstsSyncMigrator.Core/Execution/FieldMaps/FieldMergeMap.cs
@@ -17,10 +17,9 @@ namespace VstsSyncMigrator.Engine.ComponentContext
         public FieldMergeMap(FieldMergeMapConfig config)
         {
             this.config = config;
-
-            if(string.IsNullOrWhiteSpace(this.config.doneMatch))
+            if (config.targetField == config.sourceField2)
             {
-                throw new ArgumentNullException($"The {nameof(config.doneMatch)} configuration parameter cannot be null or empty.");
+                throw new ArgumentNullException($"The source field `{config.sourceField2}` can not match target field `{config.targetField}`. Please use diferent fields.");
             }
         }
 
@@ -30,18 +29,20 @@ namespace VstsSyncMigrator.Engine.ComponentContext
         {
             if (source.Fields.Contains(config.sourceField1) && source.Fields.Contains(config.sourceField2))
             {
-                if (source.Fields[config.sourceField1].Value != null)
+                var val1 = source.Fields[config.sourceField1].Value != null ? source.Fields[config.sourceField1].Value.ToString() : string.Empty;
+                var val2 = source.Fields[config.sourceField2].Value != null ? source.Fields[config.sourceField2].Value.ToString() : string.Empty;
+                var valT = target.Fields[config.targetField].Value != null ? target.Fields[config.targetField].Value.ToString() : string.Empty;
+                var newValT = string.Format(config.formatExpression, val1, val2);
+                if (valT.Contains(val2))
                 {
-                    if (target.Fields[config.targetField].Value.ToString().Contains(config.doneMatch))
+                    Trace.WriteLine(string.Format("  [SKIP] field already merged {0}:{1}+{2} to {3}:{4}", source.Id, config.sourceField1, config.sourceField2, target.Id, config.targetField));
+                } else if (valT.Equals(newValT))
                     {
-                        Trace.WriteLine(string.Format("  [SKIP] field already merged {0}:{1}+{2} to {3}:{4}", source.Id, config.sourceField1, config.sourceField2, target.Id, config.targetField));
-                    } else {
-                        var val1 = source.Fields[config.sourceField1].Value != null ? source.Fields[config.sourceField1].Value.ToString() : string.Empty;
-                        var val2 = source.Fields[config.sourceField2].Value != null ? source.Fields[config.sourceField2].Value.ToString() : string.Empty;
-
-                        target.Fields[config.targetField].Value = string.Format(config.formatExpression, val1, val2) + config.doneMatch;
-                        Trace.WriteLine(string.Format("  [UPDATE] field merged {0}:{1}+{2} to {3}:{4}", source.Id, config.sourceField1, config.sourceField2, target.Id, config.targetField));
-                    }
+                    Trace.WriteLine(string.Format("  [SKIP] field already merged {0}:{1}+{2} to {3}:{4}", source.Id, config.sourceField1, config.sourceField2, target.Id, config.targetField));
+                } else
+                {
+                    target.Fields[config.targetField].Value = newValT;
+                    Trace.WriteLine(string.Format("  [UPDATE] field merged {0}:{1}+{2} to {3}:{4}", source.Id, config.sourceField1, config.sourceField2, target.Id, config.targetField));
                 }
             }
         }
