92cda69fd654f766c9013fd4bf25dd03b22e1b79
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-09-07 17:16:48 +0100

    Moving Config data

---
+++ src/AzureDevOpsMigrationTools.Core.Configuration/FieldMap/MultiValueConditionalMapConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsMigrationTools.Core.Configuration.FieldMap
+{
+   public class MultiValueConditionalMapConfig : IFieldMapConfig
+    {
+        public string WorkItemTypeName { get; set; }
+        public Dictionary<string,string> sourceFieldsAndValues { get; set; }
+        public Dictionary<string, string> targetFieldsAndValues { get; set; }
+        public string FieldMap
+        {
+            get
+            {
+                return "MultiValueConditionalMap";
+            }
+        }
+    }
+}
---
+++ src/AzureDevOpsMigrationTools.Core.Configuration/Processing/ITfsProcessingConfig.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsMigrationTools.Core.Configuration.Processing
+{
+    public interface ITfsProcessingConfig
+    {
+        /// <summary>
+        /// Active the processor if it true. 
+        /// </summary>
+        bool Enabled { get; set; }
+        [JsonIgnoreAttribute]
+        string Processor { get; }
+        
+        /// <summary>
+        /// Indicates, if this processor can be added to the list of current processors or not.
+        /// Some processors are not compatible with each other.
+        /// </summary>
+        /// <param name="otherProcessors">List of already configured processors.</param>
+        bool IsProcessorCompatible(IReadOnlyList<ITfsProcessingConfig> otherProcessors);
+    }
+}
