b63cec594a29c545d104d70f1a4a08dd7051516e
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-09-07 10:26:13 +0100

    First Project

---
+++ src/AzureDevOpsMigrationTools.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureDevOpsMigrationTools.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
