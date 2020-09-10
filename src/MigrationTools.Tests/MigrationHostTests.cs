562ecff8484068a0f20b9f9962bab21e87a148b6
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-09-10 13:11:58 +0100

    Updated tests

--- src/MigrationTools.Tests/MigrationHostTests.cs
+++ src/MigrationTools.Tests/MigrationHostTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MigrationTools;
+using MigrationTools.Core.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,8 +13,31 @@ namespace MigrationTools.Tests
         [TestMethod()]
         public void MigrationHostTest()
         {
-            MigrationHost mh = new MigrationHost(null, null, null);
+            
+            MigrationHost mh = new MigrationHost(null, null, new EngineConfigurationBuilderStub());
 
+        }
+
+
+    }
+
+    public class EngineConfigurationBuilderStub : IEngineConfigurationBuilder
+    {
+        IEngineConfigurationBuilder ecb = new EngineConfigurationBuilder();
+
+        public EngineConfiguration BuildDefault()
+        {
+           return  ecb.BuildDefault();
+        }
+
+        public EngineConfiguration BuildFromFile()
+        {
+            return ecb.BuildDefault();
+        }
+
+        public EngineConfiguration BuildWorkItemMigration()
+        {
+            return ecb.BuildWorkItemMigration();
         }
     }
 }
