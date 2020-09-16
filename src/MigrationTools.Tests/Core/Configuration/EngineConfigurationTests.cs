420b29c14534c20f7414bc75a44b8f950b8def66
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-09-16 08:07:43 +0100

    Updated and merged all package disparity!

---
+++ src/MigrationTools.Tests/Core/Configuration/EngineConfigurationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.IO;
+using MigrationTools.Core.Configuration.FieldMap;
+using MigrationTools.Core.Configuration;
+
+namespace MigrationTools.Tests
+{
+    
+    [TestClass]
+    public class EngineConfigurationTests
+    {
+
+IEngineConfigurationBuilder ecb = new EngineConfigurationBuilder();
+        
+        [TestMethod]
+        public void TestSeraliseToJson()
+        {
+
+            string json = JsonConvert.SerializeObject(ecb.BuildDefault(),
+                    new FieldMapConfigJsonConverter(),
+                    new ProcessorConfigJsonConverter());
+            StreamWriter sw = new StreamWriter("configuration.json");
+            sw.WriteLine(json);
+            sw.Close();
+
+        }
+
+        [TestMethod]
+        public void TestDeseraliseFromJson()
+        {
+            TestSeraliseToJson();
+            EngineConfiguration ec;
+            StreamReader sr = new StreamReader("configuration.json");
+            string configurationjson = sr.ReadToEnd();
+            sr.Close();
+            ec = JsonConvert.DeserializeObject<EngineConfiguration>(configurationjson,
+                new FieldMapConfigJsonConverter(),
+                new ProcessorConfigJsonConverter());
+            Assert.AreEqual(10, ec.FieldMaps.Count);
+            Assert.AreEqual(13, ec.Processors.Count);
+        }
+    }
+}
