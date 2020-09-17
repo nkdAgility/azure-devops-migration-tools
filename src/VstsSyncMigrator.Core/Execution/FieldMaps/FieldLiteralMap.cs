e61f9f0299c33c536cf615b722e9919a1efae792
William Whittle <whittlec@users.noreply.github.com>
2020-09-17 17:44:53 +0100

    Support for mapping a literal to a field

---
+++ src/VstsSyncMigrator.Core/Execution/FieldMaps/FieldLiteralMap.cs
@@ -0,0 +1,28 @@
+﻿using MigrationTools.Core.Configuration.FieldMap;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using VstsSyncMigrator.Engine.ComponentContext;
+using System;
+
+namespace VstsSyncMigrator.Engine.ComponentContext
+{
+    class FieldLiteralMap : FieldMapBase
+    {
+        private FieldLiteralMapConfig config;
+
+        public FieldLiteralMap(FieldLiteralMapConfig config)
+        {
+            this.config = config;
+            if (config.targetField == null)
+            {
+                throw new ArgumentNullException($"The target field `{config.targetField}` must be specified. Please use diferent fields.");
+            }
+        }
+
+        public override string MappingDisplayName => $"{config.value} -> {config.targetField}";
+
+        internal override void InternalExecute(WorkItem source, WorkItem target)
+        {
+            target.Fields[config.targetField].Value = config.value;
+        }
+    }
+}
