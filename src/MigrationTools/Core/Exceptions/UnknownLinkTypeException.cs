05c9c42b9a29ec6e0c106e42af67ab1f326bd593
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-09-28 14:20:40 +0100

    Moved exceptions

--- src/MigrationTools/Core/Exceptions/UnknownLinkTypeException.cs
+++ src/MigrationTools/Core/Exceptions/UnknownLinkTypeException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VstsSyncMigrator.Engine.Execution.Exceptions
+namespace MigrationTools.Core.Exceptions
 {
     public class UnknownLinkTypeException : Exception
     {
