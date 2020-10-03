48f8332b1217994cedc478cb5cb718b36732c42b
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-10-03 13:38:00 +0100

    Removeing many Trace in favour of Log

--- src/MigrationTools.Clients.AzureDevops.Rest/FieldMaps/FieldtoFieldMultiMap.cs
+++ src/MigrationTools.Clients.AzureDevops.Rest/FieldMaps/FieldtoFieldMultiMap.cs
@@ -23,29 +23,9 @@ namespace MigrationTools.Clients.AzureDevops.Rest.FieldMaps
         internal override void InternalExecute(WorkItem source, WorkItem target)
         {
             throw new NotImplementedException();
-            //if (fieldsExist(Config.SourceToTargetMappings, source, target))
-            //    mapFields(Config.SourceToTargetMappings, source, target);
-            //else
-            //    Trace.WriteLine("  [SKIPPED] Not all source and target fields exist.");
+
         }
 
-        //private bool fieldsExist(Dictionary<string, string> fieldMap, WorkItem source, WorkItem target)
-        //{
-        //    bool exists = true;
-        //    foreach (var map in fieldMap)
-        //        if (!source.Fields.Contains(map.Key) || !target.Fields.Contains(map.Value))
-        //            exists = false;
 
-        //    return exists;
-        //}
-
-        //private void mapFields(Dictionary<string, string> fieldMap, WorkItem source, WorkItem target)
-        //{
-        //    foreach (var map in fieldMap)
-        //    {
-        //        target.Fields[map.Value].Value = source.Fields[map.Key].Value;
-        //        Trace.WriteLine(string.Format("  [UPDATE] field mapped {0}:{1} to {2}:{3}", source.Id, map.Key, target.Id, map.Value));
-        //    }
-        //}
     }
 }
