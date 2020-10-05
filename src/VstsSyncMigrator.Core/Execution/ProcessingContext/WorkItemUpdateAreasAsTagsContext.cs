141146abccd22de39ce1adf12eb3f347a8eea912
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-10-05 10:50:06 +0100

    Remove Trace from WorkItemUpdateAreasAsTagsContext

--- src/VstsSyncMigrator.Core/Execution/ProcessingContext/WorkItemUpdateAreasAsTagsContext.cs
+++ src/VstsSyncMigrator.Core/Execution/ProcessingContext/WorkItemUpdateAreasAsTagsContext.cs
@@ -21,17 +21,17 @@ namespace VstsSyncMigrator.Engine
         {
         }
 
-        public override void Configure(IProcessorConfig config)
-        {
-            this.config = (WorkItemUpdateAreasAsTagsConfig)config;
-        }
-
         public override string Name
         {
             get
             {
                 return "WorkItemUpdateAreasAsTagsContext";
             }
+        }
+
+        public override void Configure(IProcessorConfig config)
+        {
+            this.config = (WorkItemUpdateAreasAsTagsConfig)config;
         }
 
         protected override void InternalExecute()
@@ -43,7 +43,7 @@ namespace VstsSyncMigrator.Engine
             wiqb.AddParameter("AreaPath", config.AreaIterationPath);
             wiqb.Query = @"SELECT [System.Id], [System.Tags] FROM WorkItems WHERE  [System.TeamProject] = @TeamProject and [System.AreaPath] under @AreaPath";
             List<WorkItemData> workitems = Engine.Target.WorkItems.GetWorkItems(wiqb);
-            Trace.WriteLine(string.Format("Update {0} work items?", workitems.Count));
+            Log.LogInformation("Update {0} work items?", workitems.Count);
             //////////////////////////////////////////////////
             int current = workitems.Count;
             int count = 0;
@@ -52,7 +52,7 @@ namespace VstsSyncMigrator.Engine
             {
                 Stopwatch witstopwatch = Stopwatch.StartNew();
 
-                Trace.WriteLine(string.Format("{0} - Updating: {1}-{2}", current, workitem.Id, workitem.Type));
+                Log.LogInformation("{0} - Updating: {1}-{2}", current, workitem.Id, workitem.Type);
                 string areaPath = workitem.ToWorkItem().AreaPath;
                 List<string> bits = new List<string>(areaPath.Split(char.Parse(@"\"))).Skip(4).ToList();
                 List<string> tags = workitem.ToWorkItem().Tags.Split(char.Parse(@";")).ToList();
@@ -71,11 +71,11 @@ namespace VstsSyncMigrator.Engine
                 count++;
                 TimeSpan average = new TimeSpan(0, 0, 0, 0, (int)(elapsedms / count));
                 TimeSpan remaining = new TimeSpan(0, 0, 0, 0, (int)(average.TotalMilliseconds * current));
-                Trace.WriteLine(string.Format("Average time of {0} per work item and {1} estimated to completion", string.Format(@"{0:s\:fff} seconds", average), string.Format(@"{0:%h} hours {0:%m} minutes {0:s\:fff} seconds", remaining)));
+                Log.LogInformation("Average time of {0} per work item and {1} estimated to completion", string.Format(@"{0:s\:fff} seconds", average), string.Format(@"{0:%h} hours {0:%m} minutes {0:s\:fff} seconds", remaining));
             }
             //////////////////////////////////////////////////
             stopwatch.Stop();
-            Console.WriteLine(@"DONE in {0:%h} hours {0:%m} minutes {0:s\:fff} seconds", stopwatch.Elapsed);
+            Log.LogInformation("DONE in {Elapsed} seconds", stopwatch.Elapsed.ToString("c"));
         }
     }
 }
