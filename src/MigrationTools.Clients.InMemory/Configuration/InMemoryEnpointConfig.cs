b65bc02fad0ee17d854e1f98b2c821b887a1aa63
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-10-18 17:48:16 +0100

    Endpiont Mapping and config setup

---
+++ src/MigrationTools.Clients.InMemory/Configuration/InMemoryEnpointConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace MigrationTools.Configuration
+{
+    public class InMemoryWorkItemEnpointConfig : IEndpointConfig
+    {
+        public EndpointDirection Direction { get; set; }
+    }
+}
---
+++ src/MigrationTools/Configuration/EndpointConfig.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MigrationTools.Configuration
+{
+    public enum EndpointDirection
+    {
+        Source,
+        Target
+    }
+
+    public interface IEndpointConfig
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public EndpointDirection Direction { get; set; }
+    }
+}
