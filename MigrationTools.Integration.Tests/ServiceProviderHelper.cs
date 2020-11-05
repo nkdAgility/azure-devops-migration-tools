e9f73fcff47a3e3a4db0ac9b7e83d5ccad3ab170
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-11-05 08:31:03 +0000

    Updated and consolidated all packages

---
+++ MigrationTools.Integration.Tests/ServiceProviderHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using MigrationTools.Endpoints;
+using MigrationTools.EndPoints;
+using MigrationTools.Engine.Containers;
+using MigrationTools.Enrichers;
+using MigrationTools.Processors;
+
+namespace MigrationTools.Tests
+{
+    internal static class ServiceProviderHelper
+    {
+        internal static ServiceProvider GetWorkItemMigrationProcessor()
+        {
+            var logger = new NullLogger<WorkItemMigrationProcessor>();
+            var services = new ServiceCollection();
+            services.AddApplicationInsightsTelemetryWorkerService();
+
+            // Containers
+            services.AddSingleton<ProcessorContainer>();
+            services.AddSingleton<TypeDefinitionMapContainer>();
+            services.AddSingleton<GitRepoMapContainer>();
+            services.AddSingleton<FieldMapContainer>();
+            services.AddSingleton<ChangeSetMappingContainer>();
+
+            services.AddSingleton<ITelemetryLogger, TelemetryClientAdapter>();
+
+            // Processors
+            services.AddSingleton<WorkItemMigrationProcessor>();
+            services.AddTransient<ProcessorEnricherContainer>();
+            services.AddTransient<EndpointContainer>();
+
+            //Endpoints
+            services.AddTransient<InMemoryWorkItemEndpoint>();
+            services.AddTransient<EndpointEnricherContainer>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
