1844bebb63ccd2f6bcd47f816b5d9e9b7e7ba993
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-11-06 11:10:25 +0000

    Updated to remvoe AI telemitery from unit tests, and add logger to all unit tests (#697)

--- src/MigrationTools/ServiceCollectionExtensions.cs
+++ src/MigrationTools/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.ApplicationInsights.WorkerService;
 using Microsoft.Extensions.DependencyInjection;
 using MigrationTools._EngineV1.Configuration;
 using MigrationTools._EngineV1.Containers;
@@ -6,11 +7,31 @@ using MigrationTools.Configuration;
 using MigrationTools.EndPoints;
 using MigrationTools.Enrichers;
 using MigrationTools.Processors;
+using Serilog;
+using Serilog.Events;
 
 namespace MigrationTools
 {
     public static partial class ServiceCollectionExtensions
     {
+        public static void AddMigrationToolServicesForUnitTests(this IServiceCollection context)
+        {
+            // Add Fake AI Endpoint
+            var aiOptions = new ApplicationInsightsServiceOptions
+            {
+                EndpointAddress = "http://localhost:8888/v2/track"
+            };
+            context.AddApplicationInsightsTelemetryWorkerService(aiOptions);
+            context.AddSingleton<ITelemetryLogger, TelemetryClientAdapter>();
+            // Logging for Unit Tests
+            var loggers = new LoggerConfiguration().MinimumLevel.Verbose().Enrich.FromLogContext();
+            loggers.WriteTo.Logger(logger => logger
+              .WriteTo.Debug(restrictedToMinimumLevel: LogEventLevel.Verbose));
+            Log.Logger = loggers.CreateLogger();
+            Log.Logger.Information("Logger is initialized");
+            context.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+        }
+
         public static void AddMigrationToolServices(this IServiceCollection context)
         {
             //Containers
