b1728eed84e9231b9aae7866294ced95b0277e23
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-11-14 21:14:35 +0000

    Fix fail, just some tests to pass

---
+++ src/MigrationTools/Options/JsonConvertor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MigrationTools.Options
+{
+    public abstract class JsonConvertor<T> : JsonConverter
+    {
+        /// <summary>
+        /// Create an instance of objectType, based properties in the JSON object
+        /// </summary>
+        /// <param name="objectType">type of object expected</param>
+        /// <param name="jObject">
+        /// contents of JSON object that will be deserialized
+        /// </param>
+        /// <returns></returns>
+        protected abstract T Create(Type objectType, JObject jObject);
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(T).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader,
+                                        Type objectType,
+                                         object existingValue,
+                                         JsonSerializer serializer)
+        {
+            // Load JObject from stream
+            JObject jObject = JObject.Load(reader);
+
+            // Create target object based on JObject
+            T target = Create(objectType, jObject);
+
+            // Populate the object properties
+            serializer.Populate(jObject.CreateReader(), target);
+
+            return target;
+        }
+
+        public override void WriteJson(JsonWriter writer,
+                                       object value,
+                                       JsonSerializer serializer)
+        {
+            JToken jt = JToken.FromObject(value);
+            if (jt.Type != JTokenType.Object)
+            {
+                jt.WriteTo(writer);
+            }
+            else
+            {
+                JObject o = (JObject)jt;
+                o.AddFirst(new JProperty("ObjectType", value.GetType().Name));
+                o.WriteTo(writer, serializer.Converters.ToArray());
+            }
+        }
+    }
+}
