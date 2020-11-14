177b65a33ecc1c9659c6a77452505a77eddda3c2
Ove Bastiansen <obast@nets.eu>
2020-11-14 18:36:45 +0100

    Atleast it compiles now, tests failing

--- src/MigrationTools.Clients.InMemory/Endpoints/InMemoryWorkItemEndpointOptions.cs
+++ src/MigrationTools.Clients.InMemory/Endpoints/InMemoryWorkItemEndpointOptions.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using MigrationTools.EndpointEnrichers;
 
 namespace MigrationTools.Endpoints
 {
     public class InMemoryWorkItemEndpointOptions : EndpointOptions, IWorkItemEndpoint
     {
         public override Type ToConfigure => typeof(InMemoryWorkItemEndpoint);
+
+        EndpointEnricherContainer IEndpoint.EndpointEnrichers => throw new NotImplementedException();
+
+        public void Configure(IEndpointOptions options)
+        {
+            throw new NotImplementedException();
+        }
 
         public override void SetDefaults()
         {
