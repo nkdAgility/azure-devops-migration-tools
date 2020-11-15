a8f8bcfd5da9237b3fcebe7c9dbf6d2ab2bf5301
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2020-11-15 17:43:32 +0000

    Remvoing old migrators

--- src/VstsSyncMigrator.Core/Execution/MigrationContext/TeamMigrationContext.cs
+++ src/VstsSyncMigrator.Core/Execution/MigrationContext/TeamMigrationContext.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using Microsoft.Extensions.Logging;
-using Microsoft.TeamFoundation.Client;
-using Microsoft.TeamFoundation.ProcessConfiguration.Client;
 using MigrationTools;
 using MigrationTools._EngineV1.Configuration;
 using MigrationTools._EngineV1.Configuration.Processing;
-using MigrationTools._EngineV1.DataContracts;
 using MigrationTools._EngineV1.Processors;
 
 namespace VstsSyncMigrator.Engine
@@ -36,155 +30,8 @@ namespace VstsSyncMigrator.Engine
 
         protected override void InternalExecute()
         {
-            if (_config == null)
-            {
-                throw new Exception("You must call Configure() first");
-            }
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            //////////////////////////////////////////////////
-            TfsTeamService sourceTS = Engine.Source.GetService<TfsTeamService>();
-            List<TeamFoundationTeam> sourceTL = sourceTS.QueryTeams(Engine.Source.Config.AsTeamProjectConfig().Project).ToList();
-            Log.LogDebug("Found {0} teams in Source?", sourceTL.Count);
-            var sourceTSCS = Engine.Source.GetService<TeamSettingsConfigurationService>();
-            //////////////////////////////////////////////////
-            ProjectData targetProject = Engine.Target.WorkItems.GetProject();
-            Log.LogDebug("Found target project as {0}", targetProject.Name);
-            TfsTeamService targetTS = Engine.Target.GetService<TfsTeamService>();
-            List<TeamFoundationTeam> targetTL = targetTS.QueryTeams(Engine.Target.Config.AsTeamProjectConfig().Project).ToList();
-            Log.LogDebug("Found {0} teams in Target?", targetTL.Count);
-            var targetTSCS = Engine.Target.GetService<TeamSettingsConfigurationService>();
-            //////////////////////////////////////////////////
-            int current = sourceTL.Count;
-            int count = 0;
-            long elapsedms = 0;
-
-            /// Create teams
-            ///
-            foreach (TeamFoundationTeam sourceTeam in sourceTL)
-            {
-                Stopwatch witstopwatch = Stopwatch.StartNew();
-                var foundTargetTeam = (from x in targetTL where x.Name == sourceTeam.Name select x).SingleOrDefault();
-                if (foundTargetTeam == null || _config.FixTeamSettingsForExistingTeams)
-                {
-                    Log.LogDebug("Processing team '{0}':", sourceTeam.Name);
-                    TeamFoundationTeam newTeam = foundTargetTeam ?? targetTS.CreateTeam(targetProject.ToProject().Uri.ToString(), sourceTeam.Name, sourceTeam.Description, null);
-                    Log.LogDebug("-> Team '{0}' created", sourceTeam.Name);
-
-                    if (_config.EnableTeamSettingsMigration)
-                    {
-                        /// Duplicate settings
-                        Log.LogDebug("-> Processing team '{0}' settings:", sourceTeam.Name);
-                        var sourceConfigurations = sourceTSCS.GetTeamConfigurations(new List<Guid> { sourceTeam.Identity.TeamFoundationId });
-                        var targetConfigurations = targetTSCS.GetTeamConfigurations(new List<Guid> { newTeam.Identity.TeamFoundationId });
-
-                        foreach (var sourceConfig in sourceConfigurations)
-                        {
-                            if (sourceConfig.TeamSettings.BacklogIterationPath != null &&
-                                sourceConfig.TeamSettings.TeamFieldValues.Length > 0)
-                            {
-                                var targetConfig = targetConfigurations.FirstOrDefault(t => t.TeamName == sourceConfig.TeamName);
-                                if (targetConfig == null)
-                                {
-                                    Log.LogDebug("-> Settings for team '{sourceTeamName}'.. not found", sourceTeam.Name);
-                                    continue;
-                                }
-
-                                Log.LogInformation("-> Settings found for team '{sourceTeamName}'..", sourceTeam.Name);
-                                if (_config.PrefixProjectToNodes)
-                                {
-                                    targetConfig.TeamSettings.BacklogIterationPath =
-                                        string.Format("{0}\\{1}", Engine.Target.Config.AsTeamProjectConfig().Project, sourceConfig.TeamSettings.BacklogIterationPath);
-                                    targetConfig.TeamSettings.IterationPaths = sourceConfig.TeamSettings.IterationPaths
-                                        .Select(path => string.Format("{0}\\{1}", Engine.Target.Config.AsTeamProjectConfig().Project, path))
-                                        .ToArray();
-                                    targetConfig.TeamSettings.TeamFieldValues = sourceConfig.TeamSettings.TeamFieldValues
-                                        .Select(field => new TeamFieldValue
-                                        {
-                                            IncludeChildren = field.IncludeChildren,
-                                            Value = string.Format("{0}\\{1}", Engine.Target.Config.AsTeamProjectConfig().Project, field.Value)
-                                        })
-                                        .ToArray();
-                                }
-                                else
-                                {
-                                    targetConfig.TeamSettings.BacklogIterationPath = sourceConfig.TeamSettings.BacklogIterationPath;
-                                    Log.LogDebug("targetConfig.TeamSettings.BacklogIterationPath={BacklogIterationPath}", targetConfig.TeamSettings.BacklogIterationPath);
-                                    targetConfig.TeamSettings.IterationPaths = sourceConfig.TeamSettings.IterationPaths;
-                                    Log.LogDebug("targetConfig.TeamSettings.IterationPaths={@IterationPaths}", sourceConfig.TeamSettings.IterationPaths);
-                                    targetConfig.TeamSettings.TeamFieldValues = sourceConfig.TeamSettings.TeamFieldValues;
-                                    Log.LogDebug("targetConfig.TeamSettings.TeamFieldValues={@TeamFieldValues}", sourceConfig.TeamSettings.TeamFieldValues);
-                                }
-
-                                targetTSCS.SetTeamSettings(targetConfig.TeamId, targetConfig.TeamSettings);
-                                Log.LogDebug("-> Team '{0}' settings... applied", targetConfig.TeamName);
-                            }
-                            else
-                            {
-                                Log.LogWarning("-> Settings for team '{sourceTeamName}'.. not configured", sourceTeam.Name);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    Log.LogDebug("Team '{0}' found.. skipping", sourceTeam.Name);
-                }
-
-                witstopwatch.Stop();
-                elapsedms = elapsedms + witstopwatch.ElapsedMilliseconds;
-                current--;
-                count++;
-                TimeSpan average = new TimeSpan(0, 0, 0, 0, (int)(elapsedms / count));
-                TimeSpan remaining = new TimeSpan(0, 0, 0, 0, (int)(average.TotalMilliseconds * current));
-                //Log.LogInformation("Average time of {0} per work item and {1} estimated to completion", string.Format(@"{0:s\:fff} seconds", average), string.Format(@"{0:%h} hours {0:%m} minutes {0:s\:fff} seconds", remaining)));
-            }
-            // Set Team Settings
-            //foreach (TeamFoundationTeam sourceTeam in sourceTL)
-            //{
-            //    Stopwatch witstopwatch = new Stopwatch();
-            //    witstopwatch.Start();
-            //    var foundTargetTeam = (from x in targetTL where x.Name == sourceTeam.Name select x).SingleOrDefault();
-            //    if (foundTargetTeam == null)
-            //    {
-            //        TLog.LogInformation("Processing team {0}", sourceTeam.Name));
-            //        var sourceTCfU = sourceTSCS.GetTeamConfigurations((new[] { sourceTeam.Identity.TeamFoundationId })).SingleOrDefault();
-            //        TeamSettings newTeamSettings = CreateTargetTeamSettings(sourceTCfU);
-            //        TeamFoundationTeam newTeam = targetTS.CreateTeam(targetProject.Uri.ToString(), sourceTeam.Name, sourceTeam.Description, null);
-            //        targetTSCS.SetTeamSettings(newTeam.Identity.TeamFoundationId, newTeamSettings);
-            //    }
-            //    else
-            //    {
-            //        Log.LogInformation("Team found.. skipping"));
-            //    }
-
-            //    witstopwatch.Stop();
-            //    elapsedms = elapsedms + witstopwatch.ElapsedMilliseconds;
-            //    current--;
-            //    count++;
-            //    TimeSpan average = new TimeSpan(0, 0, 0, 0, (int)(elapsedms / count));
-            //    TimeSpan remaining = new TimeSpan(0, 0, 0, 0, (int)(average.TotalMilliseconds * current));
-            //    //Log.LogInformation("Average time of {0} per work item and {1} estimated to completion", string.Format(@"{0:s\:fff} seconds", average), string.Format(@"{0:%h} hours {0:%m} minutes {0:s\:fff} seconds", remaining)));
-
-            //}
-            //////////////////////////////////////////////////
-            stopwatch.Stop();
-            Log.LogDebug("DONE in {Elapsed} ", stopwatch.Elapsed.ToString("c"));
-        }
-
-        private TeamSettings CreateTargetTeamSettings(TeamConfiguration sourceTCfU)
-        {
-            ///////////////////////////////////////////////////
-            TeamSettings newTeamSettings = sourceTCfU.TeamSettings;
-            newTeamSettings.BacklogIterationPath = newTeamSettings.BacklogIterationPath.Replace(Engine.Source.Config.AsTeamProjectConfig().Project, Engine.Target.Config.AsTeamProjectConfig().Project);
-            List<string> newIterationPaths = new List<string>();
-            foreach (var ip in newTeamSettings.IterationPaths)
-            {
-                newIterationPaths.Add(ip.Replace(Engine.Source.Config.AsTeamProjectConfig().Project, Engine.Target.Config.AsTeamProjectConfig().Project));
-            }
-            newTeamSettings.IterationPaths = newIterationPaths.ToArray();
-
-            ///////////////////////////////////////////////////
-            return newTeamSettings;
+            Log.LogCritical("*************MIGRATION ALERT!!!!!!!!!");
+            Log.LogCritical("TeamMigrationContext has been migrated to TfsTeamSettingsProcessor: https://nkdagility.github.io/azure-devops-migration-tools/Reference/Processors/TfsTeamSettingsProcessor.html");
         }
     }
 }
--- src/VstsSyncMigrator.Core/Execution/MigrationContext/WorkItemQueryMigrationContext.cs
+++ src/VstsSyncMigrator.Core/Execution/MigrationContext/WorkItemQueryMigrationContext.cs
@@ -1,47 +1,19 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
 using Microsoft.Extensions.Logging;
-using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using MigrationTools;
-using MigrationTools._EngineV1.Clients;
 using MigrationTools._EngineV1.Configuration;
 using MigrationTools._EngineV1.Configuration.Processing;
 using MigrationTools._EngineV1.Processors;
 
 namespace VstsSyncMigrator.Engine
 {
+    [Obsolete("WorkItemQueryMigrationContext has been migrated to TfsSharedQueryProcessor: https://nkdagility.github.io/azure-devops-migration-tools/Reference/Processors/TfsSharedQueryProcessor.html")]
     public class WorkItemQueryMigrationContext : MigrationProcessorBase
     {
         /// <summary>
         /// The processor configuration
         /// </summary>
         private WorkItemQueryMigrationConfig config;
-
-        /// <summary>
-        /// Counter for folders processed
-        /// </summary>
-        private int totalFoldersAttempted = 0;
-
-        /// <summary>
-        /// Counter for queries attempted
-        /// </summary>
-        private int totalQueriesAttempted = 0;
-
-        /// <summary>
-        /// Counter for queries successfully migrated
-        /// </summary>
-        private int totalQueriesMigrated = 0;
-
-        /// <summary>
-        /// Counter for queries skipped
-        /// </summary>
-        private int totalQueriesSkipped = 0;
-
-        /// <summary>
-        /// Counter for the queries that failed migration
-        /// </summary>
-        private int totalQueryFailed = 0;
 
         public WorkItemQueryMigrationContext(IMigrationEngine engine, IServiceProvider services, ITelemetryLogger telemetry, ILogger<WorkItemQueryMigrationContext> logger) : base(engine, services, telemetry, logger)
         {
@@ -62,151 +34,8 @@ namespace VstsSyncMigrator.Engine
 
         protected override void InternalExecute()
         {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            //////////////////////////////////////////////////
-
-            var sourceQueryHierarchy = ((TfsWorkItemMigrationClient)Engine.Source.WorkItems).Store.Projects[Engine.Source.Config.AsTeamProjectConfig().Project].QueryHierarchy;
-            var targetQueryHierarchy = ((TfsWorkItemMigrationClient)Engine.Target.WorkItems).Store.Projects[Engine.Target.Config.AsTeamProjectConfig().Project].QueryHierarchy;
-
-            Log.LogInformation("Found {0} root level child WIQ folders", sourceQueryHierarchy.Count);
-            //////////////////////////////////////////////////
-
-            foreach (QueryFolder query in sourceQueryHierarchy)
-            {
-                MigrateFolder(targetQueryHierarchy, query, targetQueryHierarchy);
-            }
-
-            stopwatch.Stop();
-            Log.LogInformation("Folders scanned {totalFoldersAttempted}", totalFoldersAttempted);
-            Log.LogInformation("Queries Found:{totalQueriesAttempted}  Skipped:{totalQueriesSkipped}  Migrated:{totalQueriesMigrated}   Failed:{totalQueryFailed}", totalQueriesAttempted, totalQueriesSkipped, totalQueriesMigrated, totalQueryFailed);
-            Log.LogInformation("DONE in {Elapsed} seconds", stopwatch.Elapsed.ToString("c"));
-        }
-
-        /// <summary>
-        /// Define Query Folders under the current parent
-        /// </summary>
-        /// <param name="targetHierarchy">The object that represents the whole of the target query tree</param>
-        /// <param name="sourceFolder">The source folder in tree on source instance</param>
-        /// <param name="parentFolder">The target folder in tree on target instance</param>
-        private void MigrateFolder(QueryHierarchy targetHierarchy, QueryFolder sourceFolder, QueryFolder parentFolder)
-        {
-            // We only migrate non-private folders and their contents
-            if (sourceFolder.IsPersonal)
-            {
-                Log.LogInformation("Found a personal folder {sourceFolderName}. Migration only available for shared Team Query folders", sourceFolder.Name);
-            }
-            else
-            {
-                this.totalFoldersAttempted++;
-
-                // we need to replace the team project name in folder names as it included in query paths
-                var requiredPath = sourceFolder.Path.Replace($"{Engine.Source.Config.AsTeamProjectConfig().Project}/", $"{Engine.Target.Config.AsTeamProjectConfig().Project}/");
-
-                // Is the project name to be used in the migration as an extra folder level?
-                if (config.PrefixProjectToNodes == true)
-                {
-                    // we need to inject the team name as a folder in the structure
-                    requiredPath = requiredPath.Replace(config.SharedFolderName, $"{config.SharedFolderName}/{Engine.Source.Config.AsTeamProjectConfig().Project}");
-
-                    // If on the root level we need to check that the extra folder has already been added
-                    if (sourceFolder.Path.Count(f => f == '/') == 1)
-                    {
-                        var targetSharedFolderRoot = (QueryFolder)parentFolder[config.SharedFolderName];
-                        QueryFolder extraFolder = (QueryFolder)targetSharedFolderRoot.FirstOrDefault(q => q.Path == requiredPath);
-                        if (extraFolder == null)
-                        {
-                            // we are at the root level on the first pass and need to create the extra folder for the team name
-                            Log.LogInformation("Adding a folder '{Project}'", Engine.Source.Config.AsTeamProjectConfig().Project);
-                            extraFolder = new QueryFolder(Engine.Source.Config.AsTeamProjectConfig().Project);
-                            targetSharedFolderRoot.Add(extraFolder);
-                            targetHierarchy.Save(); // moved the save here a more immediate and relavent error message
-                        }
-
-                        // adjust the working folder to the newly added one
-                        parentFolder = targetSharedFolderRoot;
-                    }
-                }
-
-                // check if there is a folder of the required name, using the path to make sure it is unique
-                QueryFolder targetFolder = (QueryFolder)parentFolder.FirstOrDefault(q => q.Path == requiredPath);
-                if (targetFolder != null)
-                {
-                    Log.LogInformation("Skipping folder '{sourceFolderName}' as already exists", sourceFolder.Name);
-                }
-                else
-                {
-                    Log.LogInformation("Migrating a folder '{sourceFolderName}'", sourceFolder.Name);
-                    targetFolder = new QueryFolder(sourceFolder.Name);
-                    parentFolder.Add(targetFolder);
-                    targetHierarchy.Save(); // moved the save here a more immediate and relavent error message
-                }
-
-                // Process child items
-                foreach (QueryItem sub_query in sourceFolder)
-                {
-                    if (sub_query.GetType() == typeof(QueryFolder))
-                    {
-                        MigrateFolder(targetHierarchy, (QueryFolder)sub_query, (QueryFolder)targetFolder);
-                    }
-                    else
-                    {
-                        MigrateQuery(targetHierarchy, (QueryDefinition)sub_query, (QueryFolder)targetFolder);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Add Query Definition under a specific Query Folder.
-        /// </summary>
-        /// <param name="targetHierarchy">The object that represents the whole of the target query tree</param>
-        /// <param name="query">Query Definition - Contains the Query Details</param>
-        /// <param name="QueryFolder">Parent Folder</param>
-        private void MigrateQuery(QueryHierarchy targetHierarchy, QueryDefinition query, QueryFolder parentFolder)
-        {
-            if (parentFolder.FirstOrDefault(q => q.Name == query.Name) != null)
-            {
-                this.totalQueriesSkipped++;
-                Log.LogWarning("Skipping query '{queryName}' as already exists", query.Name);
-            }
-            else
-            {
-                // Sort out any path issues in the quertText
-                var fixedQueryText = query.QueryText.Replace($"'{Engine.Source.Config.AsTeamProjectConfig().Project}", $"'{Engine.Target.Config.AsTeamProjectConfig().Project}"); // the ' should only items at the start of areapath etc.
-
-                if (config.PrefixProjectToNodes)
-                {
-                    // we need to inject the team name as a folder in the structure too
-                    fixedQueryText = fixedQueryText.Replace($"{Engine.Target.Config.AsTeamProjectConfig().Project}\\", $"{Engine.Target.Config.AsTeamProjectConfig().Project}\\{Engine.Source.Config.AsTeamProjectConfig().Project}\\");
-                }
-
-                if (config.SourceToTargetFieldMappings != null)
-                {
-                    foreach (var sourceField in config.SourceToTargetFieldMappings.Keys)
-                    {
-                        fixedQueryText = query.QueryText.Replace(sourceField, config.SourceToTargetFieldMappings[sourceField]);
-                    }
-                }
-
-                // you cannot just add an item from one store to another, we need to create a new object
-                var queryCopy = new QueryDefinition(query.Name, fixedQueryText);
-                this.totalQueriesAttempted++;
-                Log.LogInformation("Migrating query '{queryName}'", query.Name);
-                parentFolder.Add(queryCopy);
-                try
-                {
-                    targetHierarchy.Save(); // moved the save here for better error message
-                    this.totalQueriesMigrated++;
-                }
-                catch (Exception ex)
-                {
-                    this.totalQueryFailed++;
-                    Log.LogDebug("Source Query: '{query}'");
-                    Log.LogDebug("Target Query: '{fixedQueryText}'");
-                    Log.LogError(ex, "Error saving query '{queryName}', probably due to invalid area or iteration paths", query.Name);
-                    targetHierarchy.Refresh(); // get the tree without the last edit
-                }
-            }
+            Log.LogCritical("*************MIGRATION ALERT!!!!!!!!!");
+            Log.LogCritical("WorkItemQueryMigrationContext has been migrated to TfsSharedQueryProcessor: https://nkdagility.github.io/azure-devops-migration-tools/Reference/Processors/TfsSharedQueryProcessor.html");
         }
     }
 }
