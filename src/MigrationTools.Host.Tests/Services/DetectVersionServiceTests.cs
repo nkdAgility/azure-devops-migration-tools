84eb6724ca4c904bcdc3b0922838ae9f3d41dd9c
Ben <betuttle@firstam.com>
2020-11-19 11:23:17 -0800

    Fixing + Adding test for DetectVersionService

--- src/MigrationTools.Host.Tests/Services/DetectVersionServiceTests.cs
+++ src/MigrationTools.Host.Tests/Services/DetectVersionServiceTests.cs
@@ -9,9 +9,18 @@ namespace MigrationTools.Host.Services.Tests
         [TestMethod, TestCategory("L3")]
         public void DetectVersionServiceTest()
         {
-            var dos = new DetectVersionService(new TelemetryLoggerMock());
+            var dos = new DetectVersionService(new TelemetryLoggerMock(), new CommandLine.CommonOptions());
             var result = dos.GetLatestVersion();
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod, TestCategory("L3")]
+        public void DetectVersionServiceSkipVersionCheck()
+        {   
+            var disableUpdateOption = new CommandLine.CommonOptions();
+            disableUpdateOption.SkipUpdateCheck = true;
+            var dos = new DetectVersionService(new TelemetryLoggerMock(), disableUpdateOption);
+            Assert.IsTrue(dos.SkipUpdateCheck());
         }
     }
 }
