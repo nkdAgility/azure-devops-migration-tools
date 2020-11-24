873f247cdd6f7e1e9492d87dd11b37be223a16bc
Ove Bastiansen <ovebastiansen@outlook.com>
2020-11-24 11:35:15 +0100

    Feature/command line (#769)
    * Removing usage of ExecuteOptions from domain code
    
    * Moved the hosted services to the host project
    
    * Moved all things related to commandline to the host project
    
    * Moved some unused commands for ad into the dying core project
    
    * removed not needed package references
    
    * deleted unused ad user export
    
    Co-authored-by: Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>

--- src/MigrationTools.Host/HostExtensions.cs
+++ src/MigrationTools.Host/HostExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MigrationTools.CommandLine;
+using MigrationTools.Host.CommandLine;
 
 namespace MigrationTools.Host
 {
--- src/MigrationTools/ServiceCollectionExtensions.cs
+++ src/MigrationTools/ServiceCollectionExtensions.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using Microsoft.ApplicationInsights.WorkerService;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using MigrationTools._EngineV1.Configuration;
 using MigrationTools._EngineV1.Containers;
-using MigrationTools.CommandLine;
 using MigrationTools.EndpointEnrichers;
 using MigrationTools.Endpoints;
 using MigrationTools.Enrichers;
@@ -67,26 +61,7 @@ namespace MigrationTools
             // Services
             context.AddSingleton<ITelemetryLogger, TelemetryClientAdapter>();
 
-            // Config
-            context.AddSingleton<IEngineConfigurationBuilder, EngineConfigurationBuilder>();
-            context.AddSingleton<EngineConfiguration>(sp =>
-            {
-                var executeOptions = sp.GetRequiredService<ExecuteOptions>();
-                var builder = sp.GetRequiredService<IEngineConfigurationBuilder>();
-                var logger = sp.GetServices<ILoggerFactory>().First().CreateLogger<EngineConfiguration>();
-
-                if (executeOptions.ConfigFile == string.Empty)
-                {
-                    executeOptions.ConfigFile = "configuration.json";
-                }
-                if (!File.Exists(executeOptions.ConfigFile))
-                {
-                    logger.LogInformation("The config file {ConfigFile} does not exist, nor does the default 'configuration.json'. Use '{ExecutableName}.exe init' to create a configuration file first", executeOptions.ConfigFile, Assembly.GetEntryAssembly().GetName().Name);
-                    throw new ArgumentException("missing configfile");
-                }
-                logger.LogInformation("Config Found, creating engine host");
-                return builder.BuildFromFile(executeOptions.ConfigFile);
-            });
+            
 
             // Containers
             context.AddSingleton<TypeDefinitionMapContainer>();
