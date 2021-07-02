50d26dd4504b57375b3d221abad399df03ca36a1
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2021-07-02 10:40:42 +0100

    Update TestingConstants to get the key at runtime

--- src/MigrationTools/Tests/TestingConstants.cs
+++ src/MigrationTools/Tests/TestingConstants.cs
@@ -2,7 +2,6 @@
 {
     public static class TestingConstants
     {
-        public static string AccessToken { get { return AccessTokenRaw.Replace(@"fake", ""); } }
-        public static readonly string AccessTokenRaw = "ilgcnn2fkz455l5vciz3x34fqyfake3o5hq4myjta3zoas4bfvyaljba";
+        public static readonly string AccessToken = "#{azuredevopsToken}#";
     }
 }
