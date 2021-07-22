ac32cca406220a7fd66ee5a94cf68dd20f3a89aa
jemyto <jemyto@gmail.com>
2021-07-22 18:49:31 +0300

    #969 - A fix to FieldBlankMap to work with all types and avoid invalid value assignments. (#971)
    Co-authored-by: Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>

--- src/MigrationTools.Clients.AzureDevops.ObjectModel/FieldMaps/FieldBlankMap.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/FieldMaps/FieldBlankMap.cs
@@ -16,11 +16,25 @@ namespace MigrationTools.FieldMaps.AzureDevops.ObjectModel
 
         internal override void InternalExecute(WorkItem source, WorkItem target)
         {
-            if (target.Fields.Contains(Config.targetField))
-            {
-                target.Fields[Config.targetField].Value = "";
-                Log.LogDebug("FieldBlankMap: field mapped {0}:{1} to {2} blanked", source.Id, target.Id, this.Config.targetField);
+            if (!target.Fields.Contains(Config.targetField)) {
+                Log.LogDebug($"FieldBlankMap: target field does not exist '{Config.targetField}' for '{target.Type.Name}'");
+                return;
             }
+            var targetField = target.Fields[Config.targetField];
+            if (targetField.IsLimitedToAllowedValues && targetField.AllowedValues.Count > 0 && !targetField.AllowedValues.Contains(targetField.OriginalValue as string)) {
+                Log.LogDebug($"FieldBlankMap: target field '{Config.targetField}' is limited to allowed values list: '{string.Join("', '", targetField.AllowedValues)}'");
+                return;
+            }
+            if (!targetField.IsEditable) {
+                Log.LogDebug($"FieldBlankMap: target field '{Config.targetField}' is not editable!");
+                return;
+            }
+            if (targetField.IsRequired) {
+                Log.LogDebug($"FieldBlankMap: target field '{Config.targetField}' is required!");
+                return;
+            }
+            targetField.Value = targetField.OriginalValue;
+            Log.LogDebug("FieldBlankMap: field mapped {0}:{1} to {2} blanked", source.Id, target.Id, this.Config.targetField);
         }
     }
 }
