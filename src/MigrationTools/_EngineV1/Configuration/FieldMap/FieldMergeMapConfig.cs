92b6c9c07cbe3d2fa31d788fd2a0a9d6b9e60bc1
Tom Frenzel <admin@tom-frenzel.de>
2021-07-30 10:57:13 +0200

    Allow to merge 3 instead of 2 fields into one.

--- src/MigrationTools/_EngineV1/Configuration/FieldMap/FieldMergeMapConfig.cs
+++ src/MigrationTools/_EngineV1/Configuration/FieldMap/FieldMergeMapConfig.cs
@@ -5,6 +5,7 @@
         public string WorkItemTypeName { get; set; }
         public string sourceField1 { get; set; }
         public string sourceField2 { get; set; }
+        public string sourceField3 { get; set; }
         public string targetField { get; set; }
         public string formatExpression { get; set; }
         public string doneMatch { get; set; } = "##DONE##"; //Depricated TODO remove
