f998511df546b67a32220dde2fbfe1df636c3781
ACoderLife <37108462+ACoderLife@users.noreply.github.com>
2021-08-25 01:23:54 +1000

    log api response codes (#1002)
    * log api reponse codes
    
    * change logging to improve AI submission.

--- src/MigrationTools.Clients.AzureDevops.Rest/Endpoints/AzureDevOpsEndpoint.cs
+++ src/MigrationTools.Clients.AzureDevops.Rest/Endpoints/AzureDevOpsEndpoint.cs
@@ -182,7 +182,7 @@ namespace MigrationTools.Endpoints
                         }
                         else
                         {
-                            Log.LogError($"Failed on call to get single [{typeof(DefinitionType).Name}] with Id [{definition.Id}].\r\nUrl: GET {response.RequestMessage.RequestUri.ToString()}");
+                            Log.LogError("Failed on call to get single [{definitionName}] with Id [{definitionId}].\r\nUrl: GET {requestUri}\r\nResponse Code:{statusCode}", typeof(DefinitionType).Name, definition.Id, response.RequestMessage.RequestUri.ToString(), httpResponse.StatusCode);
                             throw new Exception(await response.Content.ReadAsStringAsync());
                         }
                     }
@@ -194,7 +194,7 @@ namespace MigrationTools.Endpoints
             }
             else
             {
-                throw new Exception($"Failed on call to get list of [{typeof(DefinitionType).Name}].\r\nUrl: GET {httpResponse.RequestMessage.RequestUri.ToString()}\r\n{await httpResponse.Content.ReadAsStringAsync()}");
+                throw new Exception($"Failed on call to get list of [{typeof(DefinitionType).Name}].\r\nUrl: GET {httpResponse.RequestMessage.RequestUri.ToString()}\r\nResponse Code:{httpResponse.StatusCode}\r\n{await httpResponse.Content.ReadAsStringAsync()}");
 
             }
             return initialDefinitions;
