f9cbc12077927b6d0441add29b6ae17f303078b8
Sean Kelly <seankelly@outlook.co.nz>
2021-09-09 05:21:28 +1200

    Bug/base paths not working (#1014)
    * - Added NodeStructureEnricherEnabled to configuration
    - Enable disabling/enabling the NodeStructureEnricher via the NodeStructureEnricherEnabled config value.
    
    * fixed
    
    Co-authored-by: Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>

--- src/MigrationTools.Clients.AzureDevops.ObjectModel/ProcessorEnrichers/TfsNodeStructure.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/ProcessorEnrichers/TfsNodeStructure.cs
@@ -121,6 +121,8 @@ namespace MigrationTools.Enrichers
             {
                 newNodeName = newNodeName.Remove(newNodeName.IndexOf($@"{nodeStructureType}"), $@"{nodeStructureType}".Length);
             }
+            newNodeName = newNodeName.Replace(@"\\", @"\");
+
             return newNodeName;
         }
 
