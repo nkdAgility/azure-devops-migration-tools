e1b6af11afc5e8c7cbd969cc9a7835ad60859f75
Sean Kelly <seankelly@outlook.co.nz>
2021-09-11 18:22:40 +1200

    Clean code

--- src/MigrationTools.Clients.AzureDevops.ObjectModel/Endpoints/TfsWorkItemConvertor.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/Endpoints/TfsWorkItemConvertor.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.TeamFoundation.DistributedTask.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using MigrationTools.DataContracts;
 using Serilog;
--- src/MigrationTools.Clients.AzureDevops.ObjectModel/Endpoints/TfsWorkItemEndpoint.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/Endpoints/TfsWorkItemEndpoint.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
 using Microsoft.Extensions.Logging;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using MigrationTools.DataContracts;
