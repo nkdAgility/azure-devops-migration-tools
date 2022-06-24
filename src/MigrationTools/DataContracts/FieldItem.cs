e863f2464475ba2603dbb1d9ae2d745d2e656249
Joshua Davis <me@jdav.is>
2022-06-24 11:25:24 -0400

    fix for datetime conversion reflection issue

--- src/MigrationTools/DataContracts/FieldItem.cs
+++ src/MigrationTools/DataContracts/FieldItem.cs
@@ -1,14 +1,27 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace MigrationTools.DataContracts
 {
     public class FieldItem
     {
+        private object _value;
+
         [JsonIgnore]
         public object internalObject { get; set; }
 
         public string ReferenceName { get; set; }
         public string Name { get; set; }
-        public object Value { get; set; }
+        public object Value { 
+            get {
+                if (Name == "System.RevisedDate" && _value.ToString() == "Microsoft.TeamFoundation.WorkItemTracking.Common.ServerDefaultFieldValue")
+                    return DateTime.Parse("01/01/9999 00:00:0");
+                else
+                    return _value;
+            }
+            set {
+                _value = value;
+            }
+        }
     }
 }
