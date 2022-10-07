9bafdfd450e000ccade82c952cf653b28d858a55
Ove Bastiansen <ovebastiansen@outlook.com>
2022-10-07 19:24:18 +0200

    Minor code cleanups in the share query processor

--- src/MigrationTools.Clients.AzureDevops.ObjectModel/Processors/TfsSharedQueryProcessor.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/Processors/TfsSharedQueryProcessor.cs
@@ -13,12 +13,12 @@ namespace MigrationTools.Processors
     /// </summary>
     public class TfsSharedQueryProcessor : Processor
     {
-        private TfsSharedQueryProcessorOptions _Options;
-        private int totalFoldersAttempted;
-        private int totalQueriesAttempted;
-        private int totalQueriesSkipped;
-        private int totalQueriesMigrated;
-        private int totalQueryFailed;
+        private TfsSharedQueryProcessorOptions _options;
+        private int _totalFoldersAttempted;
+        private int _totalQueriesAttempted;
+        private int _totalQueriesSkipped;
+        private int _totalQueriesMigrated;
+        private int _totalQueryFailed;
 
         public new TfsEndpoint Source => (TfsEndpoint)base.Source;
 
@@ -37,7 +37,7 @@ namespace MigrationTools.Processors
         {
             base.Configure(options);
             Log.LogInformation("TfsSharedQueryProcessor::Configure");
-            _Options = (TfsSharedQueryProcessorOptions)options;
+            _options = (TfsSharedQueryProcessorOptions)options;
         }
 
         protected override void InternalExecute()
@@ -57,8 +57,8 @@ namespace MigrationTools.Processors
             }
 
             stopwatch.Stop();
-            Log.LogInformation("Folders scanned {totalFoldersAttempted}", totalFoldersAttempted);
-            Log.LogInformation("Queries Found:{totalQueriesAttempted}  Skipped:{totalQueriesSkipped}  Migrated:{totalQueriesMigrated}   Failed:{totalQueryFailed}", totalQueriesAttempted, totalQueriesSkipped, totalQueriesMigrated, totalQueryFailed);
+            Log.LogInformation("Folders scanned {totalFoldersAttempted}", _totalFoldersAttempted);
+            Log.LogInformation("Queries Found:{totalQueriesAttempted}  Skipped:{totalQueriesSkipped}  Migrated:{totalQueriesMigrated}   Failed:{totalQueryFailed}", _totalQueriesAttempted, _totalQueriesSkipped, _totalQueriesMigrated, _totalQueryFailed);
             Log.LogInformation("DONE in {Elapsed} seconds", stopwatch.Elapsed.ToString("c"));
         }
 
@@ -68,72 +68,73 @@ namespace MigrationTools.Processors
         /// <param name="targetHierarchy">The object that represents the whole of the target query tree</param>
         /// <param name="sourceFolder">The source folder in tree on source instance</param>
         /// <param name="parentFolder">The target folder in tree on target instance</param>
+        [Obsolete]
         private void MigrateFolder(QueryHierarchy targetHierarchy, QueryFolder sourceFolder, QueryFolder parentFolder)
         {
             // We only migrate non-private folders and their contents
             if (sourceFolder.IsPersonal)
             {
                 Log.LogInformation("Found a personal folder {sourceFolderName}. Migration only available for shared Team Query folders", sourceFolder.Name);
+                return;
+            }
+
+            _totalFoldersAttempted++;
+
+            // we need to replace the team project name in folder names as it included in query paths
+            var requiredPath = sourceFolder.Path.Replace($"{Source.Project}/", $"{Target.Project}/");
+
+            // Is the project name to be used in the migration as an extra folder level?
+            if (_options.PrefixProjectToNodes == true)
+            {
+                // we need to inject the team name as a folder in the structure
+                requiredPath = requiredPath.Replace(_options.SharedFolderName, $"{_options.SharedFolderName}/{Source.Project}");
+
+                // If on the root level we need to check that the extra folder has already been added
+                if (sourceFolder.Path.Count(f => f == '/') == 1)
+                {
+                    var targetSharedFolderRoot = (QueryFolder)parentFolder[_options.SharedFolderName];
+                    var extraFolder = (QueryFolder)targetSharedFolderRoot.FirstOrDefault(q => q.Path == requiredPath);
+                    if (extraFolder == null)
+                    {
+                        // we are at the root level on the first pass and need to create the extra folder for the team name
+                        Log.LogInformation("Adding a folder '{Project}'", Source.Project);
+                        extraFolder = new QueryFolder(Source.Project);
+                        targetSharedFolderRoot.Add(extraFolder);
+                        targetHierarchy.Save(); // moved the save here a more immediate and relavent error message
+                    }
+
+                    // adjust the working folder to the newly added one
+                    parentFolder = targetSharedFolderRoot;
+                }
+            }
+
+            // check if there is a folder of the required name, using the path to make sure it is unique
+            var targetFolder = (QueryFolder)parentFolder.FirstOrDefault(q => q.Path == requiredPath);
+            if (targetFolder != null)
+            {
+                Log.LogInformation("Skipping folder '{sourceFolderName}' as already exists", sourceFolder.Name);
             }
             else
             {
-                this.totalFoldersAttempted++;
+                Log.LogInformation("Migrating a folder '{sourceFolderName}'", sourceFolder.Name);
+                targetFolder = new QueryFolder(sourceFolder.Name);
+                parentFolder.Add(targetFolder);
+                targetHierarchy.Save(); // moved the save here a more immediate and relavent error message
+            }
 
-                // we need to replace the team project name in folder names as it included in query paths
-                var requiredPath = sourceFolder.Path.Replace($"{Source.Project}/", $"{Target.Project}/");
-
-                // Is the project name to be used in the migration as an extra folder level?
-                if (_Options.PrefixProjectToNodes == true)
+            // Process child items
+            foreach (QueryItem subQuery in sourceFolder)
+            {
+                if (subQuery.GetType() == typeof(QueryFolder))
                 {
-                    // we need to inject the team name as a folder in the structure
-                    requiredPath = requiredPath.Replace(_Options.SharedFolderName, $"{_Options.SharedFolderName}/{Source.Project}");
-
-                    // If on the root level we need to check that the extra folder has already been added
-                    if (sourceFolder.Path.Count(f => f == '/') == 1)
-                    {
-                        var targetSharedFolderRoot = (QueryFolder)parentFolder[_Options.SharedFolderName];
-                        QueryFolder extraFolder = (QueryFolder)targetSharedFolderRoot.FirstOrDefault(q => q.Path == requiredPath);
-                        if (extraFolder == null)
-                        {
-                            // we are at the root level on the first pass and need to create the extra folder for the team name
-                            Log.LogInformation("Adding a folder '{Project}'", Source.Project);
-                            extraFolder = new QueryFolder(Source.Project);
-                            targetSharedFolderRoot.Add(extraFolder);
-                            targetHierarchy.Save(); // moved the save here a more immediate and relavent error message
-                        }
-
-                        // adjust the working folder to the newly added one
-                        parentFolder = targetSharedFolderRoot;
-                    }
-                }
-
-                // check if there is a folder of the required name, using the path to make sure it is unique
-                QueryFolder targetFolder = (QueryFolder)parentFolder.FirstOrDefault(q => q.Path == requiredPath);
-                if (targetFolder != null)
-                {
-                    Log.LogInformation("Skipping folder '{sourceFolderName}' as already exists", sourceFolder.Name);
+                    MigrateFolder(targetHierarchy, (QueryFolder)subQuery, targetFolder);
                 }
                 else
                 {
-                    Log.LogInformation("Migrating a folder '{sourceFolderName}'", sourceFolder.Name);
-                    targetFolder = new QueryFolder(sourceFolder.Name);
-                    parentFolder.Add(targetFolder);
-                    targetHierarchy.Save(); // moved the save here a more immediate and relavent error message
-                }
-
-                // Process child items
-                foreach (QueryItem sub_query in sourceFolder)
-                {
-                    if (sub_query.GetType() == typeof(QueryFolder))
-                    {
-                        MigrateFolder(targetHierarchy, (QueryFolder)sub_query, (QueryFolder)targetFolder);
-                    }
-                    else
-                    {
-                        MigrateQuery(targetHierarchy, (QueryDefinition)sub_query, (QueryFolder)targetFolder);
-                    }
+                    MigrateQuery(targetHierarchy, (QueryDefinition)subQuery, targetFolder);
                 }
             }
+
         }
 
         /// <summary>
@@ -146,46 +147,44 @@ namespace MigrationTools.Processors
         {
             if (parentFolder.FirstOrDefault(q => q.Name == query.Name) != null)
             {
-                this.totalQueriesSkipped++;
+                _totalQueriesSkipped++;
                 Log.LogWarning("Skipping query '{queryName}' as already exists", query.Name);
+                return;
             }
-            else
+            // Sort out any path issues in the quertText
+            var fixedQueryText = query.QueryText.Replace($"'{Source.Project}", $"'{Target.Project}"); // the ' should only items at the start of areapath etc.
+
+            if (_options.PrefixProjectToNodes)
             {
-                // Sort out any path issues in the quertText
-                var fixedQueryText = query.QueryText.Replace($"'{Source.Project}", $"'{Target.Project}"); // the ' should only items at the start of areapath etc.
+                // we need to inject the team name as a folder in the structure too
+                fixedQueryText = fixedQueryText.Replace($"{Target.Project}\\", $"{Target.Project}\\{Source.Project}\\");
+            }
 
-                if (_Options.PrefixProjectToNodes)
+            if (_options.SourceToTargetFieldMappings != null)
+            {
+                foreach (var sourceField in _options.SourceToTargetFieldMappings.Keys)
                 {
-                    // we need to inject the team name as a folder in the structure too
-                    fixedQueryText = fixedQueryText.Replace($"{Target.Project}\\", $"{Target.Project}\\{Source.Project}\\");
+                    fixedQueryText = fixedQueryText.Replace(sourceField, _options.SourceToTargetFieldMappings[sourceField]);
                 }
+            }
 
-                if (_Options.SourceToTargetFieldMappings != null)
-                {
-                    foreach (var sourceField in _Options.SourceToTargetFieldMappings.Keys)
-                    {
-                        fixedQueryText = fixedQueryText.Replace(sourceField, _Options.SourceToTargetFieldMappings[sourceField]);
-                    }
-                }
-
-                // you cannot just add an item from one store to another, we need to create a new object
-                var queryCopy = new QueryDefinition(query.Name, fixedQueryText);
-                this.totalQueriesAttempted++;
-                Log.LogInformation("Migrating query '{queryName}'", query.Name);
-                parentFolder.Add(queryCopy);
-                try
-                {
-                    targetHierarchy.Save(); // moved the save here for better error message
-                    this.totalQueriesMigrated++;
-                }
-                catch (Exception ex)
-                {
-                    this.totalQueryFailed++;
-                    Log.LogDebug("Source Query: '{query}'");
-                    Log.LogDebug("Target Query: '{fixedQueryText}'");
-                    Log.LogError(ex, "Error saving query '{queryName}', probably due to invalid area or iteration paths", query.Name);
-                    targetHierarchy.Refresh(); // get the tree without the last edit
-                }
+            // you cannot just add an item from one store to another, we need to create a new object
+            var queryCopy = new QueryDefinition(query.Name, fixedQueryText);
+            _totalQueriesAttempted++;
+            Log.LogInformation("Migrating query '{queryName}'", query.Name);
+            parentFolder.Add(queryCopy);
+            try
+            {
+                targetHierarchy.Save(); // moved the save here for better error message
+                _totalQueriesMigrated++;
+            }
+            catch (Exception ex)
+            {
+                _totalQueryFailed++;
+                Log.LogDebug("Source Query: '{query}'");
+                Log.LogDebug("Target Query: '{fixedQueryText}'");
+                Log.LogError(ex, "Error saving query '{queryName}', probably due to invalid area or iteration paths", query.Name);
+                targetHierarchy.Refresh(); // get the tree without the last edit
             }
         }
     }
