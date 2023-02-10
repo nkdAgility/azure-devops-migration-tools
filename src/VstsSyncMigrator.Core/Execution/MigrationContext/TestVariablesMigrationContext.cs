e879660972adcaa5952ce37fa4da30600bb41487
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2023-02-10 20:33:25 +0000

    Still trying to fix that table!

--- src/VstsSyncMigrator.Core/Execution/MigrationContext/TestVariablesMigrationContext.cs
+++ src/VstsSyncMigrator.Core/Execution/MigrationContext/TestVariablesMigrationContext.cs
@@ -11,7 +11,7 @@ using VstsSyncMigrator.Engine.ComponentContext;
 namespace VstsSyncMigrator.Engine
 {
     /// <summary>
-    /// This processor can migrate test variables that are defined in the test plans / suites. This must run before `TestPlansAndSuitesMigrationConfig`
+    /// This processor can migrate test variables that are defined in the test plans / suites. This must run before `TestPlansAndSuitesMigrationConfig`. 
     /// </summary>
     /// <status>Beta</status>
     /// <processingtarget>Suites &amp; Plans</processingtarget>
