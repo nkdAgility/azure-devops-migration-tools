26ddc01d4afd5c32461fd2a7b3a2f510757fc860
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2023-03-06 10:15:09 +0000

    Update to include new field map that sets value to null

---
+++ src/MigrationTools/_EngineV1/Configuration/FieldMap/FieldClearMapConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace MigrationTools._EngineV1.Configuration.FieldMap
+{
+    /// <summary>
+    /// Allows you to blank an already populated field
+    /// </summary>
+    /// <status>ready</status>
+    /// <processingtarget>Work Item Field</processingtarget>
+    public class FieldClearMapConfig : IFieldMapConfig
+    {
+        public string WorkItemTypeName { get; set; }
+        public string targetField { get; set; }
+
+        public string FieldMap
+        {
+            get
+            {
+                return "FieldClearMap";
+            }
+        }
+
+        public void SetExampleConfigDefaults()
+        {
+            WorkItemTypeName = "*";
+            targetField = "System.Description";
+        }
+    }
+}
