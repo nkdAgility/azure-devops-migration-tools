32b5bb82dd2c34040f3daf43fdc3277c5c35dab1
Tom Frenzel <40773830+tomfrenzel@users.noreply.github.com>
2023-03-20 10:54:21 +0100

    Add build definition and task group filtering to skip them if they contain unknown tasks (#1465)

---
+++ src/MigrationTools/DataContracts/Pipelines/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MigrationTools.DataContracts.Pipelines
+{
+    [ApiPath("distributedtask/tasks", false)]
+    [ApiName("Tasks")]
+    public class TaskDefinition : RestApiDefinition
+    {
+        public override bool HasTaskGroups() => false;
+
+        public override bool HasVariableGroups() => false;
+
+        public override void ResetObject() {}
+    }
+}
