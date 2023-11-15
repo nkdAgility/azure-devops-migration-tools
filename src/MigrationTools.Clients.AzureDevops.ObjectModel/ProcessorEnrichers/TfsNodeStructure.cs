b8dd5b9288e5f82afb4d9ae704a1fefa1d4e2788
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2023-11-15 12:01:24 +0000

    Add flag for this configuration +semver: minor

--- src/MigrationTools.Clients.AzureDevops.ObjectModel/ProcessorEnrichers/TfsNodeStructure.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/ProcessorEnrichers/TfsNodeStructure.cs
@@ -259,7 +259,10 @@ namespace MigrationTools.Enrichers
             {
                 Log.LogInformation("Migrating all Nodes before the Processor run.");
                 EntryForProcessorType(processor);
-                //MigrateAllNodeStructures();
+                if (Options.ShouldCreateNodesUpFront) // FLAG for Expermiment to Do on Demand.
+                {
+                    MigrateAllNodeStructures();
+                }
                 RefreshForProcessorType(processor);
             }
         }
