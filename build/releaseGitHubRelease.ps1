b859e05ddc7759912d642ad9aa48ba27d1ed5195
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-06-22 10:39:15 +0100

    update3

--- build/releaseGitHubRelease.ps1
+++ build/releaseGitHubRelease.ps1
@@ -8,7 +8,11 @@ param (
 
     # name of the output folder
     [Parameter(Mandatory=$true)]
-    [string]$artifactFolder
+    [string]$artifactFolder,
+
+      # name of the releaseTag
+      [Parameter(Mandatory=$true)]
+      [string]$releaseTag
 )
 #Publish
 Write-Output "Azure DevOps Migration Tools (GitHub Release) Release"
@@ -16,6 +20,37 @@ Write-Output "--------------"
 $files = Get-ChildItem -Path $artifactFolder -Recurse
 foreach ($file in $files) {
     Write-Output $file.FullName
+}
+
+Write-Output "Install GitHub CLI if needed"
+if (($installedStuff -like "*gh*").Count -eq 0) {
+    Write-Output "Installing gh"
+    choco install gh --confirm --accept-license -y
+} else { Write-Output "Detected gh"}
+
+
+
+Write-Output "Release tag: $releaseTag! Version: $version"
+Write-Output "Version: $version"
+Write-Output "--------------"
+switch ($releaseTag)
+{
+    "Dev"    {
+        Write-Output "Running Dev Release"
+        #gh release create $versionText $files --generate-notes --generate-notes --prerelease --discussion-category "General"
+        }
+    "preview"    {
+        Write-Output "Running Dev Release"
+        #gh release create $versionText $files --generate-notes --generate-notes --prerelease --discussion-category "General"
+        }
+    "Release"    {
+        Write-Output "Running Dev Release"
+        #gh release create $versionText $files --generate-notes --generate-notes --prerelease --discussion-category "General"
+        }
+    default { 
+        Write-Output "Unknown Release tag of $releaseTag";
+        return -1;
+     } # optional
 }
 
 # if ($versionInfo.PreReleaseTag -eq "") {
