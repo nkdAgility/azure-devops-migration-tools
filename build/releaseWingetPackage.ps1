1a2472b7a58eecb2b9bd65cf3548fe015c2cf8d1
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-06-22 18:12:07 +0100

    Updated all release pipes!

---
+++ build/releaseWingetPackage.ps1
@@ -0,0 +1,44 @@
+<#
+    Azure DevOps Migration Tools (GitHub Release) Release
+#>
+param (
+    # height of largest column without top bar
+    [Parameter(Mandatory=$true)]
+    [string]$version,
+
+    # name of the releaseTag
+    [Parameter(Mandatory=$true)]
+    [string]$releaseTag,
+
+    # GH_TOKEN
+    [Parameter(Mandatory=$true)]
+    [string]$GH_TOKEN
+
+)
+#Publish
+Write-Output "Azure DevOps Migration Tools (Winget) Release"
+Write-Output "--------------"
+
+Write-Output "Download wingetcreate.exe"
+Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile wingetcreate.exe
+
+# echo "Structure of work folder of this pipeline:"
+# tree /f
+# Get-ChildItem -Recurse
+#https://github.com/microsoft/winget-create/blob/main/doc/update.md
+
+$installURL = "https://github.com/nkdAgility/azure-devops-migration-tools/releases/download/v$version/MigrationTools-$version.zip|x64"
+
+Write-Host $"##[warning] $installURL"
+
+$wigetPackageId = nkdAgility.AzureDevOpsMigrationTools
+if ($(releaseTag) -eq "Preview")
+{
+  $wigetPackageId = nkdAgility.AzureDevOpsMigrationTools.$Env:GITVERSION_PreReleaseLabel
+}
+
+./wingetcreate.exe update --submit --token $GH_TOKEN --urls $installURL --version $version $wigetPackageId
+
+Write-Host "Deployed : $wigetPackageId"
+
+Write-Output "--------------"
