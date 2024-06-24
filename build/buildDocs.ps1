614fc8188af929296738eaa573bb373af90ba83a
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-06-24 13:30:39 +0100

    try this

---
+++ build/buildDocs.ps1
@@ -0,0 +1,39 @@
+<#
+    Script description.
+
+    Some notes.
+#>
+param (
+    # height of largest column without top bar
+    [Parameter(Mandatory=$true)]
+    [string]$version,
+    
+    # name of the output folder
+    [Parameter(Mandatory=$true)]
+    [string]$outfolder
+)
+Write-Output "Azure DevOps Migration Tools (Documentation) Build"
+Write-Output "----------------------------------------"
+Write-Output "Version: $version"
+Write-Output "Output Folder: $outfolder"
+Write-Output "----------------------------------------"
+Write-Output "Install RubyInstallerTeam.RubyWithDevKit.3.2"
+$installedStuff = choco list -i 
+if (($installedStuff -like "*ruby*").Count -eq 0) {
+    Write-Output "Installing ruby2.devkit"
+    choco install ruby2.devkit --confirm --accept-license -y
+ } else { Write-Output "Detected ruby2.devkit"}
+
+Write-Output "----------------------------------------"
+Write-Output "gem update "
+gem update --system
+Write-Output "----------------------------------------"
+Write-Output "gem install jekyll bundler"
+gem install jekyll bundler
+Write-Output "----------------------------------------"
+Write-Output "bundle install"
+bundle install --retry=3 --jobs=4
+Write-Output "----------------------------------------"
+Write-Output "Run Jekyll Build"
+bundle exec jekyll build
+#-------------------------------------------
