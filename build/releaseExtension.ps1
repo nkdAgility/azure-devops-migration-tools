25f4dc9164a10490cd436754787743dc518dc8a2
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-07-16 12:28:32 +0100

    Update with marektplace deploy

---
+++ build/releaseExtension.ps1
@@ -0,0 +1,32 @@
+<#
+    Script description.
+
+    Some notes.
+#>
+param (    
+    # name of the output folder
+    [Parameter(Mandatory=$true)]
+    [string]$vsixFile
+    [Parameter(Mandatory=$true)]
+    [string]$marketplaceToken
+)
+Write-Output "Azure DevOps Migration Tools (Extension) Release"
+Write-Output "----------------------------------------"
+Write-Output "Extension file: $extensionFile"
+Write-Output "----------------------------------------"
+if (((npm list -g tfx-cli) -join "," ).Contains("empty")) {
+    Write-Output "Installing tfx-cli"
+    npm i -g tfx-cli
+} else { Write-Output "Detected tfx-cli"}
+Write-Output "----------------------------------------"
+# Login
+Write-Output ">>>>> Login"
+tfx login --service-url https://marketplace.visualstudio.com --token $marketplaceToken
+Write-Output "----------------------------------------"
+Write-Output "----------------------------------------"
+# Build TFS Extension
+Write-Output ">>>>> Build TFS Extension"
+tfx extension publish --vsix "$vsixFile"
+Write-Output "----------------------------------------"
+# Cleanup
+Remove-Item -Path "$outfolder/MigrationTools.Extension" -Recurse -Force
