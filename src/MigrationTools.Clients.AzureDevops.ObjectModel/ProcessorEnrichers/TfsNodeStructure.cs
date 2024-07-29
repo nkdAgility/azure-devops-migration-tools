bb2145ab3684da129018c6a7a54ed76786171c06
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2024-07-29 09:38:56 +0100

    🐛 (TfsNodeStructure.cs): replace exception with critical log and exit The change replaces the `InvalidOperationException` with a critical log message and an immediate exit using `Environment.Exit(-1)`. This ensures that the application logs the error more clearly and terminates gracefully, providing better feedback and control over the application's behavior in case of a critical failure.

--- src/MigrationTools.Clients.AzureDevops.ObjectModel/ProcessorEnrichers/TfsNodeStructure.cs
+++ src/MigrationTools.Clients.AzureDevops.ObjectModel/ProcessorEnrichers/TfsNodeStructure.cs
@@ -196,8 +196,8 @@ namespace MigrationTools.Enrichers
 
             if (parentNode == null)
             {
-                throw new InvalidOperationException(
-                    $"Path {nodePath} is not anchored in the target project, it cannot be created.");
+                Log.LogCritical($"Path {nodePath} is not anchored in the target project, it cannot be created.");
+                Environment.Exit(-1);
             }
 
             // Now that we have a parent, we can start creating nodes down the tree from that point
