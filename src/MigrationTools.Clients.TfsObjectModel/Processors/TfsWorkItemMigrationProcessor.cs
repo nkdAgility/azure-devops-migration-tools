00a2e81eb9abf41c12d3a5ae02a6d9af245ff3d8
kaflake <8384990+kaflake@users.noreply.github.com>
2025-04-02 08:44:58 +0200

    Update TfsWorkItemMigrationProcessor.cs
    ClosedDate population called two times. One time without exception handling. If there is no "Microsoft.VSTS.Common.ClosedDate" in the oldWorkItem it will crash.

--- src/MigrationTools.Clients.TfsObjectModel/Processors/TfsWorkItemMigrationProcessor.cs
+++ src/MigrationTools.Clients.TfsObjectModel/Processors/TfsWorkItemMigrationProcessor.cs
@@ -417,10 +417,6 @@ namespace MigrationTools.Processors
             }
 
             newWorkItem.Title = oldWorkItem.Title;
-            if (newWorkItem.Fields.Contains("Microsoft.VSTS.Common.ClosedDate") && newWorkItem.Fields["Microsoft.VSTS.Common.ClosedDate"].IsEditable)
-            {
-                newWorkItem.Fields["Microsoft.VSTS.Common.ClosedDate"].Value = oldWorkItem.Fields["Microsoft.VSTS.Common.ClosedDate"].Value;
-            }
             newWorkItem.State = oldWorkItem.State;
             try
             {
