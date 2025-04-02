e7076cf8f8fbbf91f7a27e8b97c19c09d6b3cee9
kaflake <8384990+kaflake@users.noreply.github.com>
2025-04-02 09:05:40 +0200

    Do not crash on CheckClosedDateIsValid if source has no closedDate

--- src/MigrationTools.Clients.TfsObjectModel/Processors/TfsWorkItemMigrationProcessor.cs
+++ src/MigrationTools.Clients.TfsObjectModel/Processors/TfsWorkItemMigrationProcessor.cs
@@ -901,7 +901,17 @@ namespace MigrationTools.Processors
             if (targetWorkItem.ToWorkItem().Fields[closedDateField].Value == null && (targetWorkItem.ToWorkItem().Fields["System.State"].Value.ToString() == "Closed" || targetWorkItem.ToWorkItem().Fields["System.State"].Value.ToString() == "Done"))
             {
                 Log.LogWarning("The field {closedDateField} is set to Null and will revert to the current date on save! ", closedDateField);
-                Log.LogWarning("Source Closed Date [#{sourceId}][Rev{sourceRev}]: {sourceClosedDate} ", sourceWorkItem.ToWorkItem().Id, sourceWorkItem.ToWorkItem().Rev, sourceWorkItem.ToWorkItem().Fields[closedDateField].Value);
+                if (sourceWorkItem.ToWorkItem().Fields.Contains(closedDateField))
+                {
+                    Log.LogWarning("Source Closed Date [#{sourceId}][Rev{sourceRev}]: {sourceClosedDate} ",
+                        sourceWorkItem.ToWorkItem().Id, sourceWorkItem.ToWorkItem().Rev,
+                        sourceWorkItem.ToWorkItem().Fields[closedDateField].Value);
+                }
+                else
+                {
+                    Log.LogWarning("Source Closed Date [#{sourceId}][Rev{sourceRev}] is not Available ",
+                        sourceWorkItem.ToWorkItem().Id, sourceWorkItem.ToWorkItem().Rev);
+                }
             }
             if (!sourceWorkItem.ToWorkItem().Fields.Contains(closedDateField))
             {
