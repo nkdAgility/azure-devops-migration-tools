8dcf3baad0c02d4e19656ac41b1590eb0718530d
Stano Gabo Peťko <petko@kros.sk>
2025-10-24 12:36:33 +0200

    Regenerate docs

--- src/MigrationTools/Tools/ExportWorkItemMappingToolOptions.cs
+++ src/MigrationTools/Tools/ExportWorkItemMappingToolOptions.cs
@@ -15,11 +15,13 @@ public class ExportWorkItemMappingToolOptions : ToolOptions
     /// <summary>
     /// Path to file, where work item mapping will be saved.
     /// </summary>
-    public string TargetFile { get; set; }
+    /// <default>String.Empty</default>
+    public string TargetFile { get; set; } = string.Empty;
 
     /// <summary>
     /// Indicates whether existing mappings in the target file should be preserved when saving new mappings.
     /// Default value is <see langword="true"/>.
     /// </summary>
+    /// <default>true</default>
     public bool PreserveExisting { get; set; } = true;
 }
